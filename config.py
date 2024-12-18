029e7830841f9cdedd9138677b9de5604b4e9693
walkerhsu <walkerhsu0808@gmail.com>
2024-12-18 20:36:17 +0800

    :bug: add more classes

--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 YOLO_MODEL_PATH = "weights/yolov8s-worldv2.pt"
-CLASSES = ["cars", "pedestrians", "stop signs", "traffic lights", "traffic signs"]
+CLASSES = [
+    "car", "truck", "bus", "motorcycle", "bicycle", "tricycle", "van", "suv", "trailer", 
+    "construction vehicle", "moped", "recreational vehicle", "pedestrian", "cyclist", 
+    "wheelchair", "stroller", "traffic light", "traffic sign", "traffic cone", 
+    "traffic island", "traffic box", "barrier", "bollard", "warning sign", "debris", 
+    "machinery", "dustbin", "concrete block", "cart", "chair", "basket", "suitcase", 
+    "dog", "phone booth"
+]
 OUTPUT_FOLDER = "results"
 TRAIN_BATCH_SIZE = 1
 VAL_BATCH_SIZE = 1
